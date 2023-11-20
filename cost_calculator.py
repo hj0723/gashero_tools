ef8d87a678511239cba41663ba737497d4ed3f9e
hj0723 <hj0723991@gmail.com>
2023-11-21 07:44:02 +0800

    Support hero upgrade cost.

---
+++ cost_calculator.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from const_cost_calculator import *
+from func_cost_calculator import *
+
+st.header('Cost Calculator :sunglasses:', divider='rainbow')
+
+st.title('Hero Upgrade Cost')
+
+hero_cost_start_level = st.number_input('Start Level', step=1, min_value=1, max_value=59)
+hero_cost_target_level = st.number_input('Target Level', step=1, min_value=2, max_value=60)
+
+st.subheader('Total Cost', divider='violet')
+pu, gmt = get_hero_cost(hero_cost_start_level, hero_cost_target_level)
+
+col1, col2= st.columns(2)
+col1.metric("Hero Potion", pu)
+col2.metric("GMT", gmt)
---
+++ func_cost_calculator.py
@@ -0,0 +1,15 @@
+from const_cost_calculator import *
+
+def get_hero_cost(hero_cost_start_level, hero_cost_target_level):
+    # (1, 2): {'pu': 1, 'gmt': 0}
+
+    pu = 0
+    gmt = 0
+    for level, cost in HERO_LEVEL_PU_COST.items():
+        level_1 = level[0]
+        level_2 = level[1]
+        if hero_cost_start_level<=level_1 and hero_cost_target_level>=level_2:
+            pu += cost['pu']
+            gmt += cost['gmt']
+
+    return pu, gmt
